7e0ace4d7ca39e19f61c99fbfa16e3b5623fe19c
Marcelo <marcelo.trajano@gmail.com>
2020-09-27 13:22:57 -0300

    update

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-  apiKey: "AIzaSyBGWycf5wFN3rXMJuMUaEYGKARnKepNi1s",
+  apiKey: "",
   authDomain: "whatsapp-badc2.firebaseapp.com",
   databaseURL: "https://whatsapp-badc2.firebaseio.com",
   projectId: "whatsapp-badc2",
   storageBucket: "whatsapp-badc2.appspot.com",
   messagingSenderId: "530754358332",
-  appId: "1:530754358332:web:825cfc1ba12c64bde80739",
+  appId: "",
 };
